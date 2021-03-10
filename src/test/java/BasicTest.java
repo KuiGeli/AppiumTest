6a0e81bbab5c3aaf2d0f9b60710eccbaaf593234
KuiGeli <gelikui11@gmail.com>
2021-03-10 13:55:40 +0200

    play as a guest

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -7,6 +7,9 @@ import org.junit.Assert;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.By;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.net.URL;
 import java.net.MalformedURLException;
 import java.util.logging.Level;
@@ -25,7 +28,7 @@ public class BasicTest {
         dc.setCapability("reportFormat", reportFormat);
         dc.setCapability("testName", testName);
         dc.setCapability(MobileCapabilityType.UDID, "JAAZGV310230MJE");
-//        dc.setCapability(MobileCapabilityType.APP, "C:\\Users\\gelik\\AppData\\Roaming\\appiumstudio\\original-apks\\com.doradogames.conflictnations.worldwar3com.bytro.sup.MainActivity.2.apk");
+        dc.setCapability(MobileCapabilityType.APP, "C:\\Users\\gelik\\AppData\\Roaming\\appiumstudio\\original-apks\\com.doradogames.conflictnations.worldwar3com.bytro.sup.MainActivity.2.apk");
         dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.doradogames.conflictnations.worldwar3");
         dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.bytro.sup.MainActivity");
         dc.setCapability("instrumentApp", true);
@@ -34,14 +37,48 @@ public class BasicTest {
     }
 
     @Test
-    public void testUntitled() {
+    public void useExistingAccount() {
         driver.context("WEBVIEW_1");
         driver.findElement(By.linkText("Use Existing Account")).click();
         driver.findElement(By.id("func_loginbutton")).click();
+
+        Assert.assertNotNull(driver.findElement(By.id("login_error_message")));
         Assert.assertTrue(driver.findElement(By.id("login_error_message")).isEnabled());
 
-        driver.findElement(By.id("func_sg_registerform_button")).click();
+//        driver.findElement(By.id("func_sg_registerform_button")).click();
+
     }
+
+    @Test
+    public void testUntitled4() {
+        driver.context("WEBVIEW_1");
+        driver.findElement(By.linkText("Use Existing Account")).click();
+        driver.findElement(By.id("func_loginbutton")).click();
+
+//        Assert.assertNotNull(driver.findElement(By.id("login_error_message")));
+        Assert.assertTrue(driver.findElement(By.id("login_error_message")).isEnabled());
+
+//        driver.findElement(By.id("func_sg_registerform_button")).click();
+
+    }
+
+//    @Test
+//    public void testPlayAsGuest() {
+//        driver.findElement(By.xpath("//*[@class='android.view.View' and @height>0 and ./*[@text='PLAY AS GUEST']]")).click();
+//        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@nodeName='DIV' and @width>0 and ./*[@text='Join random nation']]")));
+//        driver.findElement(By.xpath("//*[@text='Join random nation']")).click();
+//        driver.findElement(By.xpath("//*[@text='JOIN RANDOM NATION']")).click();
+//    }
+
+
+    @Test
+    public void playAsGuest() {
+        driver.findElement(By.xpath("//*[@class='android.view.View' and @height>0 and ./*[@text='PLAY AS GUEST']]")).click();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@nodeName='DIV' and @width>0 and ./*[@text='Join random nation']]")));
+        driver.findElement(By.xpath("//*[@nodeName='DIV' and @width>0 and ./*[@text='Join random nation']]")).click();
+    }
+
+
 
     @AfterEach
     public void tearDown() {
